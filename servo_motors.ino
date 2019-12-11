4ab88e4f3ddc7662f47cfeb5c519b7abfe1d42ba
mtthwchen <55202802+mtthwchen@users.noreply.github.com>
2019-12-11 13:21:56 -0500

    servo_motors program
    11.12.19

---
+++ servo_motors.ino
@@ -0,0 +1,19 @@
+
+#include <PRIZM.h>      // Include the PRIZM library in sketch
+
+PRIZM prizm;            // Instantiate an object in the PRIZM class named "prizm".
+
+void setup() {
+  
+  prizm.PrizmBegin();   // Initiates the PRIZM controller. Must be called at the beginning
+  prizm.setServoSpeed(1,25);
+}
+
+void loop() {
+
+  prizm.setServoPosition(1,180);        // Move servo #1 to 180 degree position. First number (#2) is ID of expansion controller.
+  delay(3000);                          // wait here for 3 seconds while servo moves to position
+  prizm.setServoPosition(1,0);          // Move servo #1 to 0 degree position. First number (#2) is ID of expansion controller.
+  delay(3000);                          // wait here for 3 seconds while servo moves to position, then repeat
+  
+}
